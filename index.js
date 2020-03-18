a0b0de08b583df50387a8820ee4892538906c119
sam <samhuntress@gmail.com>
2020-03-18 00:50:38 -0400

    Add support for authorization tag files

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ module.exports = function(root, index, req, res) {
     }
     
     if (stats.isFile()) {
-      fs.readFile(relative_pathname, function(err, data) {
+      checkAuthorization(req, res, path.dirname(relative_pathname), () => fs.readFile(relative_pathname, function(err, data) {
         if (err) {
           res.writeHead(404);
           return res.end("404 Not Found");
@@ -37,33 +37,33 @@ module.exports = function(root, index, req, res) {
         res.writeHead(200, mimeType ? {"Content-Type": mimeType} : null);
         res.write(data);
         return res.end();
-      });
+      }));
     } else if (stats.isDirectory()) {
-      fs.readdir(relative_pathname, (err, files) => {
-        const parent_dir = path.dirname(decoded_pathname);
-        res.writeHead(200);
-	
-        res.write(
+      checkAuthorization(req, res, relative_pathname, () => {
+        fs.readdir(relative_pathname, (err, files) => {
+          const parent_dir = path.dirname(decoded_pathname);
+          res.writeHead(200);
+          res.write(
 `<html>
   <head>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
       ul {
-	    	list-style-type: none;
-				display: inline-block;
+        list-style-type: none;
+        display: inline-block;
       }
     </style>
   </head>
   <body>
-		<ul>
+    <ul>
     ${parent_dir ? `<li><a href="${parent_dir}">..</a></li>`:''}
     ${files.map(file =>`<li><a href="${path.join(decoded_pathname, encodeURIComponent(file))}">${file}</a></li>`).join(`
     `)}
-		</ul>
+    </ul>
   </body>
 </html>`);
-
-        return res.end();
+          return res.end();
+        })
       })
     } else {
       res.writeHead(404);
@@ -72,6 +72,27 @@ module.exports = function(root, index, req, res) {
   });
   }
 };
+
+function checkAuthorization (req, res, checkPath, callback) {
+  fs.readFile(path.join(checkPath, ".authorized_users"), (err, data) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        callback();
+        return;
+      }
+      console.log(err);
+    }
+
+    const name = getUserName(req);
+    if (data.includes(name)) {
+      callback();
+    } else {
+      res.writeHead(403, {"Content-Type": "text/plain"});
+			res.end("Access Forbidden");
+      console.log(`unauthorized access attempt by ${name} to ${checkPath}`);
+    }
+  });
+}
 
 /**
  * This is fine.
@@ -94,3 +115,16 @@ const mimeMap = {
 	".css": "text/css",
 	".svg": "image/svg+xml"
 };
+
+/**
+ * Get current user.
+ *
+ * TODO: Fix this
+ * This should definitely go somewhere better
+ */
+function getUserName(req) {
+	const auth = req.headers.authorization;
+	const parts = auth && auth.split(' ');
+	const credentials = parts && parts.length > 1 && Buffer.from(parts[1], 'base64').toString('ascii').split(':');
+	return credentials[0];
+}
