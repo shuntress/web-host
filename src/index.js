7f432e5f1073af5c9bfde0a62f6f1b0f66dee09c
sam <samhuntress@gmail.com>
2021-02-28 16:57:18 -0500

    Add title to directory index page

--- src/index.js
+++ src/index.js
@@ -123,6 +123,7 @@ function loadDirectory(req, res, stats, webPath, absoluteSystemPath) {
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="/style.css">
 		<link rel="stylesheet" href="/layout.css">
+		<title>Index of ${webPath}</title>
 	</head>
 	<body>
 		<div class="content">
